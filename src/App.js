3fb0252889827eeb1d59a33d655ed5184b795de5
yuchi <yuchima@ym.local>
2019-08-05 22:27:46 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -6,21 +6,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <button className="button" onClick={alert} onMouseOver={change}>
+        Click
+      </button>
       </header>
     </div>
   );
+}
+
+function change(){
+  console.log('change is hovered');
 }
 
 export default App;
